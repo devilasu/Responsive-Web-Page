ca60df93ad251af59723ce394ee8638fcf5dddf4
devilasu <73906066+devilasu@users.noreply.github.com>
2021-05-13 08:20:37 +0900

    2021-05-13-0th commit

--- test/js/testjs.js
+++ test/js/testjs.js
@@ -1,31 +1,19 @@
-//jQuery CDN :Contents Delivery Network 인터넷으로 외부 프로그램을 가져다 사용. 단, 인터넷 안되는 곳 불가.
-//jQuery의 기본형식 $시작 문서가 준비되었을때 자동구현
-//인터프리터의 단점 위에서부터 아래로 실행되는 순서를 신경쓰지 않아도 된다.(jQuery는 문서 위에 있어도 괜찮다.)
-// $(document).ready(function(){});
-$(document).ready(function(){
-    $("#checkValue").click(function(){
-        alert($("#name").val());
-        $("#one").css("backgroundColor","blue");
-    })
-    // html 문서를 다 읽은 후 내용 실행.
-});
-
 //여러개의 element 선택
 var names = $("#name");
-// var names = document.getElementById("name");    //1개만 선택
+var names = document.getElementById("name");    //1개만 선택
 var inputName = document.getElementsByName("name")//1개이상 선택
 //네이밍 방식: 카멜(낙타등)표기법 소문자 시작 단어시작시 대문자
-// var className = document.getElementsByClassName("two"); //1개 이상 선택
-// console.log(className);
-// document.write("<br>")
-// document.write(className[0]["innerHTML"]);
-// document.write("<br>")
-// document.write(className[1]["innerHTML"]);
-// document.write("<br>")
-// console.log(inputName);
-// document.write(inputName[0]["defaultValue"]);
+var className = document.getElementsByClassName("two"); //1개 이상 선택
+console.log(className);
+document.write("<br>")
+document.write(className[0]["innerHTML"]);
+document.write("<br>")
+document.write(className[1]["innerHTML"]);
+document.write("<br>")
+console.log(inputName);
+document.write(inputName[0]["defaultValue"]);
 function checkValue(){
-    // alert(inputName[0]["value"]);
+    alert(inputName[0]["value"]);
     alert(names.val());
     $("#one").css("backgroundColor","red");//jQuery
     // document.getElementById("one").style.backgroundColor = "red";
